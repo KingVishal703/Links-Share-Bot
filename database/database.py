68b851f2d2c8276280c4cadce314ec1008046175
KingVishal703 <vishal21wankhede@gmail.com>
2026-03-30 18:21:34 +0530

    Add database variable for easier access

--- database/database.py
+++ database/database.py
@@ -7,6 +7,7 @@ from typing import List, Optional
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
+db = database
 
 # collections
 user_data = database['users']
