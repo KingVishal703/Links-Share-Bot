30a0d65a40e219f8c0f39acbbaf7aa38303e2481
KingVishal703 <vishal21wankhede@gmail.com>
2026-03-30 18:14:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,6 @@ from plugins import web_server
 import pyrogram.utils
 import pyromod.listen
 from aiohttp import web
-from database.database import db  # ✅ IMPORTANT
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
