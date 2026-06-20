cf973106b38bf1220a8e5776eed5ccbdfa3d5151
KingVishal703 <vishal21wankhede@gmail.com>
2026-06-20 12:49:45 +0530

    Add command to add membership for users

--- plugins/admin.py
+++ plugins/admin.py
@@ -6,6 +6,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, ChatAdminRequired, RPCError
 from database.database import set_approval_off, is_approval_off, add_admin, remove_admin, list_admins
+from database.database import add_membership
+from config import ADMINS
+
 
 @Client.on_message(filters.command("addadmin") & filters.user(OWNER_ID))
 async def add_admin_command(client, message: Message):
@@ -36,3 +39,23 @@ async def list_admins_command(client, message: Message):
         return await message.reply_text("No admins found.")
     text = "<b>Admin User IDs:</b>\n" + "\n".join([f"<code>{uid}</code>" for uid in admins])
     await message.reply_text(text)
+
+
+@Client.on_message(filters.command("addmember") & filters.user(ADMINS))
+async def add_member_cmd(client, message):
+
+    if len(message.command) != 3:
+        return await message.reply_text(
+            "Usage:\n/addmember user_id days"
+        )
+
+    user_id = int(message.command[1])
+    days = int(message.command[2])
+
+    await add_membership(user_id, days)
+
+    await message.reply_text(
+        f"✅ Membership Added\n\n"
+        f"👤 User: {user_id}\n"
+        f"⏳ Days: {days}"
+    )
